d402f02052cb24fdb0dfc9aaf2559db4a03c28fc
Aydın Türker <doktoralban@outlook.com>
2021-05-09 16:26:56 +0300

    .

--- src/app/products/products.component.ts
+++ src/app/products/products.component.ts
@@ -1,7 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
-import { $ } from 'protractor';
-import { Posts } from './posts';
+ 
 
 @Component({
   selector: 'app-products',
@@ -11,33 +10,39 @@ import { Posts } from './posts';
 export class ProductsComponent implements OnInit {
   title = 'datatables';
   dtOptions: DataTables.Settings = {};
-  Posts: any ;
+  Posts: any;
   
+
   constructor(private http: HttpClient) { }
-  
+
   ngOnInit(): void {
     this.dtOptions = {
+      'lengthMenu': [[1, 10, 25, 50, -1], [1, 10, 25, 50, 'Tümü']],
+               'paging': true,
+                 'ordering': true,
+               'order': [[1, 'desc']],
+                'info': true,
       pagingType: 'full_numbers',
       pageLength: 5,
       processing: true
       ,language: {
-               "decimal": ",",
-                "thousands": ".",
-                "lengthMenu": "Sayfada Gösterilen Satır Sayısı  _MENU_ ",
-                "zeroRecords": "Kayıt Bulunamadı!",
-                "info": "Gösterilen Sayfa  _PAGE_ / _PAGES_",
-                "infoEmpty": "Kayıt Yok!",
-                "infoFiltered": "( _MAX_ kayıttan filtrelenen)",
-                "search":"Ara"
-
+               'decimal': ',',
+                'thousands': '.',
+                'lengthMenu': 'Sayfada Gösterilen Satır Sayısı  _MENU_ ',
+                'zeroRecords': 'Kayıt Bulunamadı!',
+                'info': 'Gösterilen Sayfa  _PAGE_ / _PAGES_',
+                'infoEmpty': 'Kayıt Yok!',
+                'infoFiltered': '( _MAX_ kayıttan filtrelenen)',
+                'search':'Ara'
             }
+
     };
-  
+
     this.http.get('http://jsonplaceholder.typicode.com/posts')
-      .subscribe(posts => {
-        this.Posts = posts;
+      .subscribe(posts_ => {
+        this.Posts = posts_;
     });
-  
+
   }
-  
+
 }
